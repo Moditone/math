eefbb3c66c9d126a98f53ef9c12c6fd0769c5c85
Milan van der Meer <vandermeer.milan@gmail.com>
2016-11-25 13:20:43 +0100

    added centroid to statistics

--- statistics.hpp
+++ statistics.hpp
@@ -37,6 +37,25 @@ namespace math
     {
         return std::sqrt(meanSquare<T>(begin, end));
     }
+    
+    //! The centroid of a range of values
+    /*! The centroid or 'center of gravity' is sum of values, weighted by its index, divided by the sum of values. */
+    template <typename Iterator>
+    inline static float centroid(Iterator begin, Iterator end)
+    {
+        // Accumulation of the numerator and the denominator
+        typename Iterator::value_type numerator = 0;
+        typename Iterator::value_type denominator = 0;
+        
+        // Multiply each value with its index number and accumulate
+        for (auto index = 0; begin != end; ++begin)
+        {
+            numerator += *begin * index++;
+            denominator += *begin;
+        }
+        
+        return numerator / denominator;
+    }
 }
 
 #endif
