ea70e916f1fa926d801aa6e5ac874e6a8acb6791
Stijn Frishert <info@stijnfrishert.com>
2017-02-08 11:25:47 +0100

    Loads more easing lambda's

--- ease.hpp
+++ ease.hpp
@@ -11,30 +11,91 @@
 
 #include <cmath>
 
+#include "constants.hpp"
 #include "interpolation.hpp"
 
 namespace math
 {
-    //! Ease using linear interpolation
-    template <class T>
-    constexpr T easeLinear(const T& index)
+    //! Invert an easing function to its inverse (in/out)
+    template <typename Function>
+    auto easeInvert(Function function){ return [=](const auto x){ return 1 - function(1 - x); }; }
+    
+    //! Combine two easing functions into one ease in-out
+    template <typename Function1, typename Function2>
+    auto easeInOut(Function1 func1, Function2 func2)
     {
-        return interpolateLinear<T>(index, 0, 1);
+        return [=](const auto x)
+        {
+            return (x < 0.5) ? func1(x * 2) * 0.5 : func2((x - 0.5) * 2) * 0.5 + 0.5;
+        };
     }
     
-    //! Ease using cosine interpolation
-    template <class T>
-    constexpr T easeCosine(const T& index)
+    //! Turn a single easing function into one ease in-out
+    template <typename Function>
+    auto easeInOut(Function function)
     {
-        return interpolateCosine<T>(index, 0, 1);
+        return easeInOut(function, easeInvert(function));
     }
     
-    //! Ease using
-    template <class T>
-    constexpr T easeCircular(const T& index)
+    // Linear
+    static const auto easeLinear = [](const auto x){ return x; };
+    
+    // Exponential forms
+    static const auto easeQuadraticIn = [](const auto x) { return x * x; };
+    static const auto easeQuadraticOut = easeInvert(easeQuadraticIn);
+    static const auto easeQuadraticInOut = easeInOut(easeQuadraticIn);
+    static const auto easeQuadraticOutIn = easeInOut(easeQuadraticOut);
+    
+    static const auto easeCubicIn = [](const auto x) { return x * x * x; };
+    static const auto easeCubicOut = easeInvert(easeCubicIn);
+    static const auto easeCubicInOut = easeInOut(easeCubicIn);
+    static const auto easeCubicOutIn = easeInOut(easeCubicOut);
+    
+    static const auto easeQuarticIn = [](const auto x) { return x * x * x * x; };
+    static const auto easeQuarticOut = easeInvert(easeQuarticIn);
+    static const auto easeQuarticInOut = easeInOut(easeQuarticIn);
+    static const auto easeQuarticOutIn = easeInOut(easeQuarticOut);
+    
+    static const auto easeQuinticIn = [](const auto x) { return x * x * x * x * x; };
+    static const auto easeQuinticOut = easeInvert(easeQuinticIn);
+    static const auto easeQuinticInOut = easeInOut(easeQuinticIn);
+    static const auto easeQuinticOutIn = easeInOut(easeQuinticOut);
+
+    inline auto easeExponentialIn(double exponent) { return [=](const auto x){ return std::pow(x, exponent); }; }
+    inline auto easeExponentialOut(double exponent) { return easeInvert(easeExponentialIn); }
+    inline auto easeExponentialInOut(double exponent) { return easeInOut(easeExponentialIn(exponent)); }
+    inline auto easeExponentialOutIn(double exponent) { return easeInOut(easeExponentialOut(exponent)); }
+    
+    // Circular
+    static const auto easeCircularIn = [](const auto x) { return 1 - std::sqrt(1 - (x * x)); };
+    static const auto easeCircularOut = easeInvert(easeCircularIn);
+    static const auto easeCircularInOut = easeInOut(easeCircularIn);
+    static const auto easeCircularOutIn = easeInOut(easeCircularOut);
+    
+    // Sine/cosine
+    static const auto easeSineOut = [](const auto x){ return std::sin(x * HALF_PI<decltype(x)>); };
+    static const auto easeSineIn = easeInvert(easeSineOut);
+    static const auto easeSineInOut = easeInOut(easeSineIn);
+    static const auto easeSineOutIn = easeInOut(easeSineOut);
+    
+    // Elastic
+    inline auto easeElasticOut(double p)
     {
-        return 1 - std::sqrt(1 - (index * index));
+        return [=](const auto x)
+        {
+            return std::pow(2, -10 * x) * std::sin((x - p / 4) * TWO_PI<decltype(x)> / p) + 1;
+        };
     }
+    
+    inline auto easeElasticIn(double p) { return easeInvert(easeElasticOut(p)); }
+    inline auto easeElasticInOut(double p) { return easeInOut(easeElasticIn(p)); }
+    inline auto easeElasticOutIn(double p) { return easeInOut(easeElasticOut(p)); }
+    
+    // Back
+    inline auto easeBackIn(double s) { return [=](const auto x) { return x * x * ((s + 1) * x - s); }; }
+    inline auto easeBackOut(double p) { return easeInvert(easeBackIn(p)); }
+    inline auto easeBackInOut(double p) { return easeInOut(easeBackIn(p)); }
+    inline auto easeBackOutIn(double p) { return easeInOut(easeBackOut(p)); }
 }
 
 #endif /* MATH_EASE_HPP */
