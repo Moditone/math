8122412d9b1dd08eedb3db99391c37a29c5514e8
Milan van der Meer <vandermeer.milan@gmail.com>
2017-03-28 12:48:41 +0200

    crossCorrelation uses convolve functions with reversed iterators for the kernel

--- crossCorrelation.hpp
+++ crossCorrelation.hpp
@@ -11,42 +11,22 @@
 
 #include <iterator>
 
+#include "convolution.hpp"
+
 namespace math
 {
     //! Cross-correlate two buffers, return a buffer with size input + kernel - 1 (output-side algorithm)
     template <typename InputIterator, typename KernelIterator>
     static std::vector<std::common_type_t<typename InputIterator::value_type, typename KernelIterator::value_type>> crossCorrelate(InputIterator inBegin, InputIterator inEnd, KernelIterator kernelBegin, KernelIterator kernelEnd)
     {
-        auto inputSize = std::distance(inBegin, inEnd);
-        auto kernelSize = std::distance(kernelBegin, kernelEnd);
-        auto size = inputSize + kernelSize - 1;
-        
-        vector<float> output(size);
-        for (auto i = 0 ; i < size; i++)
-            for (auto h = 0; h < kernelSize; h++)
-            {
-                if (i - h < 0) continue;
-                else if (i - h >= inputSize) continue;
-                output[i] += kernelBegin[kernelSize - 1 - h] * inBegin[i - h];
-            }
-        
-        return output;
+        return convolve(inBegin, inEnd, std::make_reverse_iterator(kernelEnd), std::make_reverse_iterator(kernelBegin));
     }
     
     //! Cross-correlate two buffers, return a buffer with size input + kernel - 1 (input-side algorithm)
     template <typename InputIterator, typename KernelIterator>
     static std::vector<std::common_type_t<typename InputIterator::value_type, typename KernelIterator::value_type>> crossCorrelateInputSide(InputIterator inBegin, InputIterator inEnd, KernelIterator kernelBegin, KernelIterator kernelEnd)
     {
-        auto inputSize = std::distance(inBegin, inEnd);
-        auto kernelSize = std::distance(kernelBegin, kernelEnd);
-        auto size = inputSize + kernelSize - 1;
-        
-        vector<float> output(size);
-        for (auto i = 0; i < inputSize; i++)
-            for (auto h = 0; h < kernelSize; h++)
-                output[i+h] += kernelBegin[kernelSize - 1 - h] * inBegin[i];
-        
-        return output;
+        return convolveInputSide(inBegin, inEnd, std::make_reverse_iterator(kernelEnd), std::make_reverse_iterator(kernelBegin));
     }
 }
 
