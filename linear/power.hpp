9aa79933f27b3e09c596049061e420a51c3726a4
Stijn Frishert <stijnfrishert@gmail.com>
2018-11-27 14:03:46 +0100

    Vector powers

--- linear/power.hpp
+++ linear/power.hpp
@@ -6,8 +6,8 @@
 //  Licensed under the BSD 3-clause license.
 //
 
-#ifndef MATH_LINEAR_MODULO_HPP
-#define MATH_LINEAR_MODULO_HPP
+#ifndef MATH_LINEAR_POWER_HPP
+#define MATH_LINEAR_POWER_HPP
 
 #include <algorithm>
 #include <cmath>
@@ -18,11 +18,11 @@
 namespace math
 {
     template <typename T1, typename T2, std::size_t N>
-    auto modulo(const Vector<T1, N>& lhs, const Vector<T2, N>& rhs)
+    auto power(const Vector<T1, N>& lhs, const Vector<T2, N>& rhs)
     {
         using T = std::common_type_t<T1, T2>;
         Vector<T, N> result;
-        std::transform(lhs.begin(), lhs.end(), rhs.begin(), result.begin(), [&](auto& a, auto& b){ return std::fmod(a, b); });
+        std::transform(lhs.begin(), lhs.end(), rhs.begin(), result.begin(), [&](auto& a, auto& b){ return std::pow(a, b); });
         return result;
     }
 }
@@ -30,9 +30,9 @@ namespace math
 namespace std
 {
     template <typename T1, typename T2, std::size_t N>
-    auto fmod(const math::Vector<T1, N>& lhs, const math::Vector<T2, N>& rhs)
+    auto pow(const math::Vector<T1, N>& lhs, const math::Vector<T2, N>& rhs)
     {
-        return modulo(lhs, rhs);
+        return power(lhs, rhs);
     }
 }
 
